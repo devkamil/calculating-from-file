a94cc59c0f7fa6b2029857872003945425a515cf
devkamil <kamilkarwowski.dev@gmail.com>
2018-09-16 23:01:32 +0200

    new 'final static' value and small fix in 'while' condition

--- src/main/java/pl/devkamil/app/InputLoader.java
+++ src/main/java/pl/devkamil/app/InputLoader.java
@@ -6,15 +6,16 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class InputLoader {
+    private final static String APPLY = TypeOfCalculation.APPLY.name();
 
     public ArrayList<String> readFile(String pathToFile){
         ArrayList<String> listWithValuesFromFile = new ArrayList<>();
         String line;
         boolean isFindWordApply = false;
         try(BufferedReader bufferedReader = new BufferedReader(new FileReader(pathToFile))){
-            while ( ((line = bufferedReader.readLine()) != null) && isFindWordApply==false) {
+            while ( ((line = bufferedReader.readLine()) != null) && !isFindWordApply) {
                 listWithValuesFromFile.add(line);
-                if (line.toUpperCase().contains("APPLY")){
+                if (line.toUpperCase().contains(APPLY)){
                     isFindWordApply = true;
                 }
             }
